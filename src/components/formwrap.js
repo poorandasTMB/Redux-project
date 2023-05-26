5a10a930bcd2662af46fc31aab8fca151db89d1d
Pooran Das <pooran.d@cubiclogics.com>
2023-05-26 17:38:45 +0530

    saasd

--- src/components/formwrap.js
+++ src/components/formwrap.js
@@ -1,10 +1,8 @@
-
 // Import Header
 import Header from "./formheader";
 import Inputcontroler from './inputcontroler';
 import { useDispatch, useSelector } from 'react-redux';
 import { update } from '../redux/formSlice';
-
 
 let inputGroupList = [
     {
@@ -40,23 +38,22 @@ let inputGroupList = [
         },
         inputGroup: [
             {
-                name: 'step',
-                type: "text",
-                label: "Name123456",
-                placeholder: "Enter your name"
+                id: "standard",
+                name: 'radio',
+                type: "radio",
+                label: "standard",
             },
             {
-                name: 'email2ssd',
-                type: "email",
-                label: "Emailwee567",
-                placeholder: "Enter your email"
-            }
-            ,
+                id: "medium",
+                name: 'radio',
+                type: "radio",
+                label: "Medium",
+            },
             {
-                name: 'email2ssd',
-                type: "email",
-                label: "Emailwee567",
-                placeholder: "Enter your email"
+                id: "high",
+                name: 'radio',
+                type: "radio",
+                label: "High",
             }
         ]
     },
@@ -89,53 +86,39 @@ export default function Formwrapper() {
     const dispatch = useDispatch();
     const formData = useSelector((state) => state.formReduxData);
     const handlesubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        dispatch(update({ name: "formStep", value: formData.formStep + 1 }))
     }
 
     return (
-        <div className="form">
+        <form className="form" onSubmit={handlesubmit}>
             {
                 formData.formStep <= inputGroupList.length ?
                     <>
                         <Header headData={inputGroupList[formData.formStep - 1].headData} />
-                        <form onSubmit={handlesubmit}>
-                            {inputGroupList[formData.formStep - 1].inputGroup.map((item, index) => {
-                                console.log(inputGroupList, formData.formStep - 1)
-                                return <Inputcontroler inputData={item} key={index} />
-                            })}
-                        </form>
+                        {inputGroupList[formData.formStep - 1].inputGroup.map((item, index) => {
+                            return <Inputcontroler inputData={item} key={index} />
+                        })}
                     </>
                     :
                     <>
                         <table>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Phone number</th>
-                            </tr>
-                            <tr>
-                                <td>{formData?.name}</td>
-                                <td> {formData?.email}</td>
-                                <td> {formData?.PhoneNumber}</td>
-                                
-                            </tr>
-                        
+                            {Object.entries(formData).map((item) => {
+                                if (item[0] === "formStep") return ""
+                                return (
+                                    <tr>
+                                        <td>{item[0]}</td>
+                                        <td>{item[1]}</td>
+                                    </tr>
+                                )
+                            })}
                         </table>
-{/* 
-
-                        Name : {formData?.name}<br></br>
-                        Email : {formData?.email}<br></br>
-                        step : {formData?.step}<br></br>
-                        email2 : {formData?.email2}<br></br> */}
                     </>
             }
             <div className='form_btn'>
                 {formData.formStep > 1 && <button type='button' onClick={(e) => { dispatch(update({ name: "formStep", value: formData.formStep - 1 })) }}>Prev Step</button>}
-                {formData.formStep <= inputGroupList.length && <button type='button' onClick={(e) => { dispatch(update({ name: "formStep", value: formData.formStep + 1 })) }}>Next Step</button>}
+                {formData.formStep <= inputGroupList.length && <button type='submit'>Next Step</button>}
             </div>
-
-        </div>
+        </form>
     )
-
-
 }
--- src/components/inputcontroler.js
+++ src/components/inputcontroler.js
@@ -2,14 +2,26 @@ import { useSelector, useDispatch } from 'react-redux';
 // import { update } from '../../store/formSlice';
 import { update } from '../redux/formSlice';
 
-export default function Inputcontroler({ inputData: { name, type, label, placeholder } }) {
+export default function Inputcontroler({inputData: { id, name, type, label, placeholder } }) {
     const dispatch = useDispatch();
     const formData = useSelector((state) => state.formReduxData);
+
+    let inputTypes;
+
+  switch (type) {
+    case 'radio':
+        inputTypes = <input required id={id} name={name} placeholder={placeholder} type={type} value={label} checked={formData[name] === label} onChange={(e) => { dispatch(update({ name: e.target.name, value: e.target.value })) }} />
+      break;
+    default:
+        inputTypes = <input required id={id} name={name} placeholder={placeholder} type={type} value={formData[name] || ""} onChange={(e) => { dispatch(update({ name: e.target.name, value: e.target.value })) }} />
+      break;
+  }
+
     return (
         <>
             <div className="input_box">
-                <label htmlFor={name}>{label}</label>
-                <input required id={name} name={name} placeholder={placeholder} type={type} value={formData[name]||""} onChange={(e) => { dispatch(update({ name: e.target.name, value: e.target.value })) }} />
+                <label htmlFor={id}>{label}</label>
+                {inputTypes}
             </div>
         </>
     )
